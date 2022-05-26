ae353a6a4bd4fd6d25bef00dd53f24075fb3e353
gmzi <gmzieres@gmail.com>
2022-05-26 12:34:44 -0400

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react';
 import Layout from '../components/layout'
